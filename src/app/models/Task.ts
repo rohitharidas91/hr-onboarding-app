bede346751ec5c715a5768643507f1d9f15c3cdc
Rohit H <rohitharidas91@gmail.com>
2025-11-19 15:47:45 +0400

    Implemented user schema and task schema + connected mongo db

---
+++ src/app/models/Task.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const taskSchema = new Schema(
+  {
+    title: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      required: true,
+    },
+    dueDate: {
+      type: Date,
+      required: true,
+    },
+    startDate: {
+      type: String,
+      required: true,
+    },
+    completionDate: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Task || mongoose.model("Task", taskSchema);
---
+++ src/app/models/User.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    designation: {
+      type: String,
+      required: true,
+    },
+    department: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", userSchema);
